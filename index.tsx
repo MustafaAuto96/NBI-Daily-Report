4c52a54536d330ecb3fbe7fe174774a68e270f05
MustafaAuto96 <flashvg88@gmail.com>
2025-09-30 00:06:12 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; 
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -10,5 +11,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
+    <React.StrictMode> <BrowserRouter basename="/NBI-Daily-Report">
+      <App /> 
+    </BrowserRouter>
   </React.StrictMode>
 );
